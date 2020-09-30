143c8c6d6eba93df2772a505a622177d890f6ab8
ericamendez <ecmendez25@gmail.com>
2020-09-30 10:26:27 -0400

    added the rootReducer

--- src/reducers/rootReducer.ts
+++ src/reducers/rootReducer.ts
@@ -1,1 +1,14 @@
-export const rootReducer = {}
+import { combineReducers } from "redux"
+
+const initState = {}
+
+const reducer = (state: any = initState, action: any) => {
+  switch(action.type){
+    default:
+      return state
+  }
+}
+
+export const rootReducer = combineReducers({
+  app: reducer
+})
