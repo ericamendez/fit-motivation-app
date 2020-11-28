f027983802453f82dee37db3bc5b46dc9dbce17d
vanely <vanelys94@gmail.com>
2020-11-27 22:47:12 -0500

    import name correction

--- server.js
+++ server.js
@@ -1,15 +1,16 @@
 const webpackHotMiddleware = require('webpack-hot-middleware');
-const webpackMiddleware = require('webpack-dev-middleware');
+const webpackDevMiddleware = require('webpack-dev-middleware');
 const webpackconfig = require('./webpack.config')
 const webpack = require('webpack');
 const express = require('express');
 const app = express();
 
-const port = process.env || 3300;
+const port = process.env.PORT || 3300;
+console.log("Node Process Object: ", process)
 app.use(express.static('dist'));
 
 const webpackCompiler = webpack(webpackconfig);
-app.use(webpackMiddleware(webpackCompiler, {}));
+app.use(webpackDevMiddleware(webpackCompiler, {}));
 app.use(webpackHotMiddleware(webpackCompiler));
 app.listen(port, () => {
   console.log(`App listening on port: ${port}!`)
