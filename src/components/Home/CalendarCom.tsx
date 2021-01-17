8ae2fb91413f528288957c31ff4dfe85ad3b51dc
ericamendez <ecmendez25@gmail.com>
2021-01-17 14:22:38 -0500

    calender tileContent compenent manipulation

--- src/components/Home/CalendarCom.tsx
+++ src/components/Home/CalendarCom.tsx
@@ -1,15 +1,47 @@
-import React, {Component} from 'react'
-import Calendar from 'react-calendar'
+import React, { Component } from "react";
+import Calendar from "react-calendar";
+import TileContent from "./TileContent";
 import "react-calendar/dist/Calendar.css";
 
-class CalendarCom extends Component {
-    render() {
-        return (
-            <div>
-                <Calendar></Calendar>
-            </div>
-        )
-    }
+interface IContentData {
+  activeStartDate?: any;
+  date: any;
+  view: any;
 }
 
-export default CalendarCom
+const CONTENT_DATA: IContentData = {
+  activeStartDate: new Date(Date.now()),
+  date: new Date(Date.now()),
+  view: "month",
+};
+
+class CalendarCom extends Component {
+  handleClickDay = (e: any) => {
+    console.log(e);
+  };
+
+  contentDate = ({ activeStartDate, date, view }: any) => {
+    console.log("activeStartDate", activeStartDate);
+    console.log("view", view);
+    console.log("date", date.getDate());
+    console.log("date.now", Date.now());
+    // return <TileContent/>
+    
+    return date.getDate() === new Date(Date.now()).getDate() ? <TileContent /> : null;
+  };
+
+  render() {
+    return (
+      <div>
+        <Calendar
+          onChange={this.handleClickDay}
+          value={new Date(Date.now())}
+          tileContent={this.contentDate}
+          view={'month'}
+        />
+      </div>
+    );
+  }
+}
+
+export default CalendarCom;
---
+++ src/components/Home/TileContent.tsx
@@ -0,0 +1,11 @@
+import React from 'react'
+
+function TileContent(img) {
+  return (
+    <div>
+      <img src={img}/>
+    </div>
+  )
+}
+
+export default TileContent
